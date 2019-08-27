c50f4816d425b526af788177cad00ff5a789c39e
Chris Ha <chunghha@users.noreply.github.com>
2019-08-26 21:44:36 -0500

    chore: update theme

--- lib/styling.dart
+++ lib/styling.dart
@@ -3,23 +3,22 @@ import 'package:flutter/material.dart';
 class AppTheme {
   AppTheme._();
 
-  static const Color primaryColor = Color(0xFF005DAA);
-  static const Color accentColor = Color(0xFFDA291C);
+  static const Color primaryColor = Color(0xFF5E81AC);
+  static const Color accentColor = Color(0xFFBF616A);
 
-  static const Color orange = Color(0xFFFAAB1A);
-  static const Color notWhite = Color(0xFFEDF0F2);
-  static const Color nearlyWhite = Color(0xFFFEFEFE);
-  static const Color nearlyBlack = Color(0xFF213333);
+  static const Color notWhite = Color(0xFFE5E9F0);
+  static const Color nearlyWhite = Color(0xFFECEFF4);
+  static const Color nearlyBlack = Color(0xFF3B4252);
   static const Color grey = Color(0xFF3A5160);
   static const Color dark_grey = Color(0xFF313A44);
 
-  static const Color darkText = Color(0xFF253840);
-  static const Color darkerText = Color(0xFF17262A);
-  static const Color lightText = Color(0xFF4A6572);
+  static const Color darkText = Color(0xFF3B4252);
+  static const Color darkerText = Color(0xFF2E3440);
+  static const Color lightText = Color(0xFF434C52);
   static const Color deactivatedText = Color(0xFF767676);
-  static const Color dismissibleBackground = Color(0xFF364A54);
-  static const Color chipBackground = Color(0xFFEEF1F3);
-  static const Color spacer = Color(0xFFF2F2F2);
+  static const Color dismissibleBackground = Color(0xFF4C566A);
+  static const Color chipBackground = Color(0xFFD8DEE9);
+  static const Color spacer = Color(0xFFE5E9F0);
 
   static const TextTheme textTheme = TextTheme(
     display1: display1,
